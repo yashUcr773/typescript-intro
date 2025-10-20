fe81e826e77b7b5fc36c722af858838dd035113a
YashCompro1 <yash.aggarwal@comprotechnologies.com>
2025-10-20 12:32:14 +0530

    basic todo app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,33 @@
-console.log('test')
+interface Todo {
+    title: string;
+    completed: boolean;
+}
+
+const todos: Todo[] = JSON.parse(localStorage.getItem('todos') || '[]')
+
+todos.forEach(todo => renderTodo(todo))
+
+document.querySelector('#add-todo')?.addEventListener('click', () => {
+    const todoInput = (document.querySelector('#todo-input') as HTMLInputElement)
+    const title = todoInput.value;
+    todoInput.value = "";
+    const todo: Todo = { title, completed: false }
+    todos.push(todo)
+    localStorage.setItem('todos', JSON.stringify(todos))
+    renderTodo(todo)
+})
+
+function renderTodo(todo: Todo) {
+    const todoContainer = document.querySelector('#todos-container');
+    const todoElement = document.createElement('li')
+    todoElement.innerText = todo.title
+    const todoCheckbox = document.createElement('input')
+    todoCheckbox.type = 'checkbox'
+    todoCheckbox.addEventListener('click', function (e) {
+        todo.completed = (e.target as HTMLFormElement).checked
+        localStorage.setItem('todos', JSON.stringify(todos))
+    })
+    todoCheckbox.checked = todo.completed
+    todoElement.append(todoCheckbox)
+    todoContainer?.appendChild(todoElement)
+}
